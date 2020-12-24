eef633dc063f76188b26e6d00da16fd847d18b63
Tanaka Kensaku <tannakaken@gmail.com>
2020-12-25 02:31:13 +0900

    ソース分割の仕組みと自動コンパイルの仕組みを作成

---
+++ .github/actions/latex/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eux
+
+# build pdf (change if necessary)
+platex main.tex
+platex main.tex
+dvipdfmx main.dvi
+
+# create release
+res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
+-d "
+{
+  \"tag_name\": \"v$GITHUB_SHA\",
+  \"target_commitish\": \"$GITHUB_SHA\",
+  \"name\": \"v$GITHUB_SHA\",
+  \"draft\": false,
+  \"prerelease\": false
+}"`
+
+# extract release id
+rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
+
+# upload built pdf
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=main.pdf\
+  --header 'Content-Type: application/pdf'\
+  --upload-file main.pdf
+
